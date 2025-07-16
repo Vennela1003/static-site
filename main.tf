667d505cae52a8b8d7c1bc30f756ba77c690e456
vennela <gvennelaraju@gmail.com>
2025-07-16 11:57:21 +0000

    secondcommit

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.static_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
